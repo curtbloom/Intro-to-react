5acd39ef1053083e565aa9d5c1752f2df92b1eca
Curtis Bloomfield <curtbloom@gmail.com>
2018-12-31 16:10:43 -0500

    CP4 finished

--- src/App.js
+++ src/App.js
@@ -1,56 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import ToDo from './components/ToDo.js';
-
-const data = [
-  {
-    id: '1',
-    toDo: 'Wash the car',
-    done: true,
-  },
-  {
-    id: '2',
-    toDo: 'Walk the dog',
-    done: false,
-  },
-  {
-    id: '3',
-    toDo: 'Go to the groceries',
-    done: false,
-  },
-]
+ import ToDo from './components/ToDo.js';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      todos: data,
-    }
-  }
-
   render() {
     return (
       <div className="App">
         <ul>
-          {
-            this.state.todos.map((todo) => {
-              return (
-                <ToDo toDo={todo} />
-              )
-            })
-          }
+          <ToDo />
+          <ToDo />
         </ul>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
       </div>
     );
   }
 }
+
 export default App;
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,25 +1,11 @@
  import React, { Component } from 'react';
 
 class ToDo extends Component {
-  renderDone() {
-    if (this.props.toDo.done) {
-      return (
-        <span>x</span>
-      )
-    }
-
-    return (
-      <span>o</span>
-    )
-  }
-
   render() {
-    return (
-      <div>
-        <li>{this.props.toDo.toDo}</li>
-        {this.renderDone()}
-      </div>
-    );
-  }
+     return (
+       <li> A todo will go here </li>
+     );
+   }
 }
+
 export default ToDo;
